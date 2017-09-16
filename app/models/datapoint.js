838ac64ce235654823309b8dec03b66bd4759042
Frank Soucy <franksoucy9@gmail.com>
2017-09-16 17:55:47 -0400

    Added the datapoint model

---
+++ app/models/datapoint.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Key = require('./key');
+
+module.exports = mongoose.model('Datapoint', new Schema({
+    filename: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    labelled: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    label: {
+	type: String,
+	required: true,
+	default: "No label"
+    },
+    latitude: {
+	type: Double,
+	required: true
+    },
+    longitude: {
+	type: Double,
+	required: true
+    },
+    keys: [Schema(Key)]
+}));
